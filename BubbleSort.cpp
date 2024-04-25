7f6620b8dfbde6eb2cecebd5400e8f0476639361
Omer Yassir <omerym10@gmail.com>
2024-04-25 23:57:02 +0200

    dffdh

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -5,12 +5,14 @@ using namespace std;
 //function<bool(const Student&, const Student&)> byGpa = [](const Student& s1, const Student& s2) {return s1.getGpa() < s2.getGpa(); };
 //Bubble_Sort(students, size,byGpa);
 template <typename T>
-void Bubble_Sort(T* arr, int Size, function<bool(const T&, const T&)> lessThan)
+int Bubble_Sort(T* arr, int Size, function<bool(const T&, const T&)> lessThan)
 {
+	int comparisons = 0;
 	for (int i = Size; i >= 0; i--)
 	{
 		for (int j = 1; j < i; j++)
 		{
+			comparisons++;
 			if (lessThan(arr[j], arr[j - 1]))
 			{
 				T temp = arr[j - 1];
@@ -19,11 +21,11 @@ void Bubble_Sort(T* arr, int Size, function<bool(const T&, const T&)> lessThan)
 			}
 		}
 	}
+	return comparisons;
 }
 template <typename T>
-void Bubble_Sort(T* arr, int Size)
+int Bubble_Sort(T* arr, int Size)
 {
 	function<bool(const T&, const T&)> lessThan = [](const T& t1, const T& t2)->bool { return (t1 < t2); };
-
-	Bubble_Sort(arr, Size, lessThan);
+	return Bubble_Sort(arr, Size, lessThan);
 }
