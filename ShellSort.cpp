ae6dbfc785a3917d70c7b03728ace30b254506e6
Omar-Salah123 <129892168+Omar-Salah123@users.noreply.github.com>
2024-04-25 22:53:00 +0200

    shell sort added

---
+++ ShellSort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <functional>
+using namespace std;
+template <typename T>
+void Shell_Sort(T* arr, int Size, function<bool(const T&, const T&)> lessThan)
+{
+	for (int gap = Size / 2; gap > 0; gap /= 2)
+	{
+		for (int i = Size - 1; i - gap >= 0; i--)
+		{
+			if (arr[i] < arr[i - gap])
+			{
+				int temp = arr[i];
+				arr[i] = arr[i - gap];
+				arr[i - gap] = temp;
+
+			}
+
+		}
+	}
+}
+template <typename T>
+void Shell_Sort(T* arr, int Size)
+{
+	function<bool(const T&, const T&)> lessThan = [](const T& t1, const T& t2)->bool { return (t1 < t2); };
+
+	Shell_Sort(arr, Size, lessThan);
+}
+
