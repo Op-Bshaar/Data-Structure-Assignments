36b86a99898b7329e6a248ed136ca66e8c13f857
Omar-Salah123 <129892168+Omar-Salah123@users.noreply.github.com>
2024-04-25 23:32:21 +0200

    Update ShellSort.cpp

--- ShellSort.cpp
+++ ShellSort.cpp
@@ -7,17 +7,19 @@ int Shell_Sort(T* arr, int Size, function<bool(const T&, const T&)> lessThan)
 	int comparisons = 0;
 	for (int gap = Size / 2; gap > 0; gap /= 2)
 	{
-		for (int i = Size - 1; i - gap >= 0; i--)
-		{
-			comparisons++;
-			if (lessThan(arr[i], arr[i - gap]))
-			{
-				T temp = arr[i];
-				arr[i] = arr[i - gap];
-				arr[i - gap] = temp;
+	    for (int i = 0; i+ gap <Size; i++)
+	    {
+		    for (int i = 0; i+ gap <Size; i+=gap)
+		        {
+			        comparisons++;
+			    if (lessThan(arr[i + gap],arr[i]))
+			    {
+				    T temp = arr[i];
+				    arr[i] = arr[i + gap];
+				    arr[i + gap] = temp;
 
-			}
-
+			    }
+		        }
 		}
 	}
 	return comparisons;
@@ -28,4 +30,3 @@ int Shell_Sort(T* arr, int Size)
 	function<bool(const T&, const T&)> lessThan = [](const T& t1, const T& t2)->bool { return (t1 < t2); };
 	return Shell_Sort(arr, Size, lessThan);
 }
-
