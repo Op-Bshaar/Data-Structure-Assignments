83836ddea94214871fa2c722fab85b412f37383d
Omer Yassir <omerym10@gmail.com>
2024-04-25 22:21:35 +0200

    quickSort

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,4 +1,5 @@
 #include<functional>
+#include<iostream>
 using namespace std;
 
 template <typename T>
@@ -13,18 +14,30 @@ void quickSort(T* arr, int size, function<bool(const T&, const T&)> lessThan)
 	quickSort(arr, 0, size - 1, lessThan);
 }
 template <typename T>
-void quickSort(T* arr, int bigen,int end, function<bool(const T&, const T&)> lessThan)
+void quickSort(T* arr, int begin,int end, function<bool(const T&, const T&)> lessThan)
 {
 	if (begin >= end)
 	{
 		return;
 	}
-	int p = pivot(arr, begin, end, lessThan);
+	int p = partition(arr, begin, end, lessThan);
 	quickSort(arr, begin, p - 1, lessThan);
 	quickSort(arr, p + 1, end, lessThan);
 }
 template <typename T>
-int pivot(T* arr, int bigen, int end, function<bool(const T&, const T&)> lessThan)
+int partition(T* arr, int begin, int end, function<bool(const T&, const T&)> lessThan)
 {
-	return 0;
+	int pivot_index = begin;
+	T pivot = arr[pivot_index];
+	int i = begin;
+	for (int j = begin; j <= end; j++)
+	{
+		if (lessThan(arr[j], pivot))
+		{
+			i++;
+			swap(arr[i], arr[j]);
+		}
+	}
+	swap(arr[i], arr[pivot_index]);
+	return i;
 }
