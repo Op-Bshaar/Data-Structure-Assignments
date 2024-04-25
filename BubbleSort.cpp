b5754e873c3cd8a4c15e5bd594367a398f4a758e
Omar-Salah123 <129892168+Omar-Salah123@users.noreply.github.com>
2024-04-25 22:58:37 +0200

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,19 +1,12 @@
+#include <iostream>
 #include <functional>
 using namespace std;
 
 //function<bool(const Student&, const Student&)> byGpa = [](const Student& s1, const Student& s2) {return s1.getGpa() < s2.getGpa(); };
 //Bubble_Sort(students, size,byGpa);
 template <typename T>
-int Bubble_Sort(T* arr, int Size)
+void Bubble_Sort(T* arr, int Size, function<bool(const T&, const T&)> lessThan)
 {
-	function<bool(const T&, const T&)> lessThan = [](const T& t1, const T& t2)->bool { return (t1 < t2); };
-
-	return Bubble_Sort(arr, Size, lessThan);
-}
-template <typename T>
-int Bubble_Sort(T* arr, int Size, function<bool(const T&, const T&)> lessThan)
-{
-	int comparisons = 0;
 	for (int i = Size; i >= 0; i--)
 	{
 		for (int j = 1; j < i; j++)
@@ -24,8 +17,13 @@ int Bubble_Sort(T* arr, int Size, function<bool(const T&, const T&)> lessThan)
 				arr[j - 1] = arr[j];
 				arr[j] = temp;
 			}
-			comparisons++;
 		}
 	}
-	return comparisons;
+}
+template <typename T>
+void Bubble_Sort(T* arr, int Size)
+{
+	function<bool(const T&, const T&)> lessThan = [](const T& t1, const T& t2)->bool { return (t1 < t2); };
+
+	Bubble_Sort(arr, Size, lessThan);
 }
