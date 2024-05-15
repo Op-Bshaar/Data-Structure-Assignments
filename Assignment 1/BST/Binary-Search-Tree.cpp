2a88089930e8bec46526a955a06dd9c60516239f
Omar-Salah123 <sd.omar04@gmail.com>
2024-05-15 17:57:04 +0300

    starting on trees

---
+++ Assignment 1/BST/Binary-Search-Tree.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <functional>
+using namespace std;
+template <typename T>
+public class node
+{
+	private node* right;
+	private node* left;
+	private T Item
+		Node(T thing, node* Right)
+	{
+		this.right = right;
+		this.Item = thing;
+	}
+	T GetItem()
+	{
+		return Item;
+	}
+};
+public class BinarySearchTree
+{
+	private node Root;
+	private node Current;
+	BinarySearchTree(node start)
+	{
+		this.Root = start;
+	}
+	Push(node Node)
+	{
+	while(Node->next!=NULL)
+	{
+
+		current = Node->next;
+	}
+
+	}
+}
+;
